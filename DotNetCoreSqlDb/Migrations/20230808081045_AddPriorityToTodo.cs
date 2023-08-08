c0fd9ca6c05ead89a148072c695a37fe3bc1465f
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-08 08:13:17 +0000

    Migration

---
+++ DotNetCoreSqlDb/Migrations/20230808081045_AddPriorityToTodo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriorityToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Todo",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Todo");
+        }
+    }
+}
