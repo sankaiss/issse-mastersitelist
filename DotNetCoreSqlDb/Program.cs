a8214c362cfe06971eb806bf3cb65322e6599dc2
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-09 11:42:23 +0000

    V5

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -63,7 +63,7 @@ app.MapControllerRoute(
 
 app.Run();
 
-static static async Task EnsureRolesCreated(IServiceProvider serviceProvider)
+private static async Task EnsureRolesCreated(IServiceProvider serviceProvider)
 {
     var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
