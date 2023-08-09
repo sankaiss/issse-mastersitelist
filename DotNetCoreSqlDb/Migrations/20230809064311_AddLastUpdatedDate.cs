b09f8b61ef7004dd84c562a6234ef3e8be361fdc
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-09 06:43:26 +0000

    Migrate

---
+++ DotNetCoreSqlDb/Migrations/20230809064311_AddLastUpdatedDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastUpdatedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedDate",
+                table: "Site",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedDate",
+                table: "Site");
+        }
+    }
+}
