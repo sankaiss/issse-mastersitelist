dd1a2406c5629ba968a5114384857c2cb95aced6
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-09 18:52:22 +0000

    V05

--- DotNetCoreSqlDb/Services/EmailService.cs
+++ DotNetCoreSqlDb/Services/EmailService.cs
@@ -3,30 +3,31 @@ using Azure.Security.KeyVault.Secrets;
 using Microsoft.Extensions.Configuration;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System;
 using System.Threading.Tasks;
 
 namespace DotNetCoreSqlDb.Services
 {
-public class EmailService : IEmailService
-{
-    private readonly ISendGridClient _sendGridClient;
-
-    public EmailService(IConfiguration configuration)
+    public class EmailService : IEmailService
     {
-        var keyVaultUrl = configuration.GetValue<string>("AzureKeyVaultEndpoint");
-        var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-        KeyVaultSecret sendGridKey = client.GetSecret("SendGridApiKey");
-        string sendGridApiKey = sendGridKey.Value;
+        private readonly ISendGridClient _sendGridClient;
 
-        _sendGridClient = new SendGridClient(sendGridApiKey);
-    }
+        public EmailService(IConfiguration configuration)
+        {
+            var keyVaultUrl = configuration.GetValue<string>("AzureKeyVaultEndpoint");
+            var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+            KeyVaultSecret sendGridKey = client.GetSecret("SendGridApiKey");
+            string sendGridApiKey = sendGridKey.Value;
 
-    public async Task SendEmailAsync(string toEmail, string subject, string content)
-    {
-        var from = new EmailAddress("IT-PortalenAdmin@se.issworld.com", "MasterSiteList Admin");
-        var to = new EmailAddress(toEmail);
-        var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
-        await _sendGridClient.SendEmailAsync(msg);
-    }
-}
+            _sendGridClient = new SendGridClient(sendGridApiKey);
+        }
 
+        public async Task SendEmailAsync(string toEmail, string subject, string content)
+        {
+            var from = new EmailAddress("IT-PortalenAdmin@se.issworld.com", "MasterSiteList Admin");
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            await _sendGridClient.SendEmailAsync(msg);
+        }
+    } 
+} 
