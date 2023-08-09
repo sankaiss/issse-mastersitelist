f29c83d5298dfc0de0b894391805c4d13d0a8a84
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-09 14:53:27 +0000

    24

--- DotNetCoreSqlDb/Controllers/AccountController.cs
+++ DotNetCoreSqlDb/Controllers/AccountController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using DotNetCoreSqlDb.ViewModel;
+using DotNetCoreSqlDb.Models;
 
 
 public class AccountController : Controller
