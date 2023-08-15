700b193a632254969f023b050d8e6b39b28f23d6
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-15 14:06:15 +0000

    999

--- DotNetCoreSqlDb/Models/Site.cs
+++ DotNetCoreSqlDb/Models/Site.cs
@@ -12,14 +12,14 @@ namespace DotNetCoreSqlDb.Models
         public string? GammalAdressEfterFlytt { get; set; }
         public string? Leverantör { get; set; }
         public string? Status { get; set; }
-        public int? NätverkskapacitetMbps { get; set; }
+        public string? NätverkskapacitetMbps { get; set; }
         public int? NätverkskapacitetGbps { get; set; } 
         public string? KontaktNamn { get; set; }
         public string? ISSKontorSite { get; set; }
         public string? Mobilnr { get; set; }
         public string? Epostadress { get; set; }
         public string? WANUplink { get; set; }
-        public int? AntalEnheter { get; set; }
+        public string? AntalEnheter { get; set; }
         public string? Sitestorlek { get; set; }
         public string? Kommentarer { get; set; }
         public DateTime LastUpdatedDate { get; set; }
