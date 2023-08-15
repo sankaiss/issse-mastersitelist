ac83c4e98c2fb5f54634506159a561cb43e96883
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-15 20:20:39 +0000

    sany

---
+++ DotNetCoreSqlDb/Migrations/20230815202022_AddEtablering.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEtablering : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NätverkskapacitetMbps",
+                table: "Site",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AntalEnheter",
+                table: "Site",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NätverkskapacitetMbps",
+                table: "Site",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AntalEnheter",
+                table: "Site",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ DotNetCoreSqlDb/Models/Etablering.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreSqlDb.Models
+{
+
+
+    public class Etablering
+    {
+        public int Id { get; set; }
+        public string? KontorSite { get; set; }
+        public string? AteaKonsult { get; set; }
+        public string? Gatuadress { get; set; }
+        public string? Postnr { get; set; }
+        public string? Ort { get; set; }
+        public string? ISSKontaktperson { get; set; }
+        public string? EpostISSKontaktperson { get; set; }
+        public string? TelefonISSKontaktperson { get; set; }
+        public string? TeliaUppkoppling { get; set; }
+        public string? OvrigInfo { get; set; }
+        public string? KlartSenast { get; set; }
+        public string? Status { get; set; }
+    }
+}
