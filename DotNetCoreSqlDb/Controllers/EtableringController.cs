9f419fad1bb39dbb75d67f01ec34399f82c17270
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-15 21:43:57 +0000

    etablering2

--- DotNetCoreSqlDb/Controllers/EtableringController.cs
+++ DotNetCoreSqlDb/Controllers/EtableringController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Data;
 using DotNetCoreSqlDb.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetCoreSqlDb.Controllers
 {
