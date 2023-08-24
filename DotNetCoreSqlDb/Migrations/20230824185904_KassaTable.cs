5083ad296c1497abd849172a8e61686877f34e7e
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-08-24 19:01:19 +0000

    migr

---
+++ DotNetCoreSqlDb/Migrations/20230824185904_KassaTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class KassaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KontorSite",
+                table: "Kassas");
+
+            migrationBuilder.RenameColumn(
+                name: "ÄrendeNrISS",
+                table: "Kassas",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Kassas",
+                newName: "ÄrendeNrISS");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KontorSite",
+                table: "Kassas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
