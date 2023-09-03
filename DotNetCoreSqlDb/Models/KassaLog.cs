f53cf13d456c1eda0d1dbabe3d6cd3a4cb4830b2
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-09-03 15:02:39 +0000

    fggfg

---
+++ DotNetCoreSqlDb/Models/KassaLog.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCoreSqlDb.Models
+{
+public class KassaLog
+{
+    public int Id { get; set; }
+    public int PrinterId { get; set; }
+    public string? PropertyName { get; set; }
+    public string? OldValue { get; set; }
+    public string? NewValue { get; set; }
+    public DateTime ChangeDate { get; set; }
+    public string? UserId { get; set; }
+    public string? UserName { get; set; }
+}
+}
