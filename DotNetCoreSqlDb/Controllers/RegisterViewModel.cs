5be727d054ef8e3ff84cfc885631afed49847e2c
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-09-07 15:32:10 +0000

    23323

--- DotNetCoreSqlDb/Controllers/RegisterViewModel.cs
+++ DotNetCoreSqlDb/Controllers/RegisterViewModel.cs
@@ -2,20 +2,23 @@ using System.ComponentModel.DataAnnotations;
 
 public class RegisterViewModel
 {
-    [Required(ErrorMessage = "Please enter your name.")]
+    [Required(ErrorMessage = "Ange ditt namn.")]
     [Display(Name = "Ditt Namn")]
     public string FullName { get; set; }
 
-    [Required(ErrorMessage = "Please enter an email.")]
-    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    [Required(ErrorMessage = "Ange en e-postadress.")]
+    [EmailAddress(ErrorMessage = "Ogiltig e-postadress.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "Please enter a password.")]
+    [Required(ErrorMessage = "Ange ett lösenord.")]
     [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).+$", ErrorMessage = "Lösenordet måste innehålla minst en liten bokstav, en stor bokstav, en siffra och ett specialtecken.")]
+    [MinLength(8, ErrorMessage = "Lösenordet måste vara minst 8 tecken långt.")]
     public string Password { get; set; }
 
-    [Required(ErrorMessage = "Please confirm your password.")]
+    [Required(ErrorMessage = "Bekräfta lösenordet.")]
     [DataType(DataType.Password)]
-    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    [Compare("Password", ErrorMessage = "Lösenorden matchar inte.")]
     public string ConfirmPassword { get; set; }
+
 }
