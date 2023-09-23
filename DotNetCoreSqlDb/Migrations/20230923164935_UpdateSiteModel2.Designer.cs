8202820ce9d57cda87bc8cf7dbfe66c988c5ca5c
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-09-23 16:57:23 +0000

    hfdhdhf

--- DotNetCoreSqlDb/Migrations/20230923164935_UpdateSiteModel2.Designer.cs
+++ DotNetCoreSqlDb/Migrations/20230923164935_UpdateSiteModel2.Designer.cs
@@ -4,6 +4,7 @@ using DotNetCoreSqlDb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    partial class MyDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230923164935_UpdateSiteModel2")]
+    partial class UpdateSiteModel2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -375,18 +378,13 @@ namespace DotNetCoreSqlDb.Migrations
 
                     b.Property<string>("SiteId")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)")
-                        .HasColumnName("SiteID");
+                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
 
                     b.HasIndex("SiteID");
 
-                    b.ToTable("SiteImages", t =>
-                        {
-                            t.Property("SiteID")
-                                .HasColumnName("SiteID1");
-                        });
+                    b.ToTable("SiteImages");
                 });
 
             modelBuilder.Entity("DotNetCoreSqlDb.Models.SiteLog", b =>
--- DotNetCoreSqlDb/Models/SiteImage.cs
+++ DotNetCoreSqlDb/Models/SiteImage.cs
@@ -1,18 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema; // Importera detta namespace
 
 namespace DotNetCoreSqlDb.Models
 {
-public class SiteImage
-{
-    public int Id { get; set; }
+    public class SiteImage
+    {
+        public int Id { get; set; }
 
-    [Required]
-    public string? SiteId { get; set; } // Länka bilden till en specifik Site med ett ID
+        [Required]
+        [Column("SiteID")] // Ange det faktiska kolumnnamnet i din databas
+        public string? SiteId { get; set; } // Länka bilden till en specifik Site med ett ID
 
-    [Required]
-    public string? ImageUrl { get; set; } // Länken till den uppladdade bilden
+        [Required]
+        public string? ImageUrl { get; set; } // Länken till den uppladdade bilden
 
-    // Lägg till fler egenskaper om det behövs (t.ex. beskrivning, datum, osv.)
-}
+        // Lägg till fler egenskaper om det behövs (t.ex. beskrivning, datum, osv.)
+    }
 }
