69b9d62935f78d41eb44d176eb4b6c4bbd79e986
sankaiss <137890308+sankaiss@users.noreply.github.com>
2023-10-21 20:12:16 +0000

    65656

---
+++ DotNetCoreSqlDb/Migrations/20231021201202_AddImageNew33334.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageNew33334 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageFileName",
+                table: "Site",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Site",
+                newName: "ImageFileName");
+        }
+    }
+}
